dde16e87a7303ff2a9196beb6dc821b975f5f37b
Ivan Ustyantsev <vangold915@gmail.com>
2022-12-26 16:57:35 +0700

    Completed task 1

--- task1.py
+++ task1.py
@@ -4,6 +4,22 @@
 """
 
 
+# Первый пример
+class RoadAttrValidator:
+    """Класс для валидации отрицательных значений"""
+    def __init__(self, my_attr):
+        self.my_attr = my_attr
+
+    def __set__(self, instance, value):
+        if isinstance(value, int) and isinstance(value, float):
+            raise ValueError(f'Значение {self.my_attr} должно иметь тип int'
+                             f' или float')
+        if value < 0:
+            raise ValueError(f'Значение {self.my_attr} не может быть'
+                             f' отрицательным!')
+        instance.__dict__[self.my_attr] = value
+
+
 class Road:
     """
     Класс для создания объектов дорог
@@ -11,9 +27,12 @@ class Road:
     single_asphalt_mass = 25
     height_road = 5
 
+    length = RoadAttrValidator('length')
+    width = RoadAttrValidator('width')
+
     def __init__(self, length, width):
-        self._length = length
-        self._width = width
+        self.length = length
+        self.width = width
 
     def calc_mass_asphalt(self):
         """
@@ -21,27 +40,77 @@ class Road:
         дорожного полотна
         :return: строка результата
         """
-        road_mass = round(self._width * self._length *
+        road_mass = round(self.width * self.length *
                           self.single_asphalt_mass * self.height_road / 1000)
-        return f'Масса асфальта, для дороги\nдлиной - {self._length} ' \
-               f'метров,\nшириной - {self._width} метров,\n' \
+        return f'Масса асфальта, для дороги\nдлиной - {self.length} ' \
+               f'метров,\nшириной - {self.width} метров,\n' \
                f'высотой полотна - {self.height_road} сантиметров\n' \
                f'равна {road_mass} тонн'
 
 
 print('--------- Класс Road ---------')
-m54_first_part = Road(5000, -20)
+m54_first_part = Road(5000, 20)
+# m54_first_part = Road(-5000, 20)
+# m54_first_part = Road(5000, -20)
+# m54_first_part = Road("string", 20)
+# m54_first_part = Road(5000, "string")
 print(m54_first_part.calc_mass_asphalt())
 
 m54_first_part.length = 5000
+# m54_first_part.length = -5000
 m54_first_part.width = 20
+# m54_first_part.width = -20
 print(m54_first_part.calc_mass_asphalt())
 print('')
+
+
+# Второй пример
+class NumValueValidator:
+    """Класс для валидации числовых значений"""
+    def __init__(self, my_attr):
+        self.my_attr = my_attr
+
+    def __set__(self, instance, value):
+        if not isinstance(value, int) and not isinstance(value, float):
+            raise ValueError(f'Значение {self.my_attr} должно иметь тип int'
+                             f' или float!')
+        if value < 0:
+            raise ValueError(f'Значение {self.my_attr} должно быть'
+                             f' положительным!')
+        instance.__dict__[self.my_attr] = value
+
+
+class StrValueValidator:
+    """Класс для валидации строковых значений"""
+    def __init__(self, my_attr):
+        self.my_attr = my_attr
+
+    def __set__(self, instance, value):
+        if not isinstance(value, str):
+            raise ValueError(f'Значение {self.my_attr} должно иметь тип str!')
+        instance.__dict__[self.my_attr] = value
+
+
+class BoolValueValidator:
+    """Класс для валидации булевых значений"""
+    def __init__(self, my_attr):
+        self.my_attr = my_attr
+
+    def __set__(self, instance, value):
+        if not isinstance(value, bool):
+            raise ValueError(f'Значение {self.my_attr} должно иметь тип bool!')
+        instance.__dict__[self.my_attr] = value
+
 
 class Car:
     """
     Класс машин
     """
+    speed = NumValueValidator('speed')
+    color = StrValueValidator('color')
+    name = StrValueValidator('name')
+    is_police = BoolValueValidator('is_police')
+
     def __init__(self, speed, color, name, is_police):
         self.speed = speed
         self.color = color
@@ -84,14 +153,14 @@ class Car:
 
 
 print('--------- Класс Car ---------')
-toyota_ist = Car(-55, 'красная', 'toyota ist', False)
+toyota_ist = Car(55, 'красная', 'toyota ist', False)
+# toyota_ist = Car(55.5, 'красная', 'toyota ist', False)
+# toyota_ist = Car(-55, 'красная', 'toyota ist', False)
+# toyota_ist = Car('string', 'красная', 'toyota ist', False)
+# toyota_ist = Car(55, 50, 'toyota ist', False)
+# toyota_ist = Car(55, 'красная', True, False)
+# toyota_ist = Car(55, 'красная', 'toyota ist', 123)
 print(toyota_ist.name)
 print(toyota_ist.speed)
 print(toyota_ist.color)
 print(toyota_ist.is_police)
-print(toyota_ist.to_go())
-print(toyota_ist.stop())
-print(toyota_ist.turn('лево'))
-print(toyota_ist.turn('право'))
-print(toyota_ist.show_speed())
-print(toyota_ist.show_status())
